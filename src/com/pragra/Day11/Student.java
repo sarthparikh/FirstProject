8c77030cfc7100269e32c5e0e7812a9a153c1010
sarthparikh <sarthparikh@yahoo.com>
2022-11-19 13:12:47 -0500

    hi

--- src/com/pragra/Day11/Student.java
+++ src/com/pragra/Day11/Student.java
@@ -1,11 +1,39 @@
 package com.pragra.Day11;
 
-import com.pragra.Day12.Circle;
+public class Student extends Object {
+    // the very first line in any constructor is either a call to super() or a call to this()
+    long id;
+    String name;
+    int grade;
 
-public class Student {
-    public static void main(String[] args) {
-        Circle c=new Circle();
-        c.CalcCircumference();
+    public Student(){
+super();
+        System.out.println("Default Constructor");
+    }
+    public  Student(int id){
+        this();
+        System.out.println("In constructor 1");
+        this.id=id;
+    }
+   public  Student(int id, String name){
+        this(id);
+       System.out.println("In constructor 2");
+        this.name=name;
+    }
+    public  Student(int id, String name, int grade){
+        this(id,name);
+        System.out.println("In Constructor 3");
+        this.grade=grade;
+    }
+
+    public void study ()
+    {
+
+        System.out.println("I am studying"+id);
+    }
+    public void play ()
+    {
+        System.out.println(id);
     }
 }
 
--- src/com/pragra/Day12/Constructor.java
+++ src/com/pragra/Day12/Constructor.java
@@ -1,8 +1,12 @@
 package com.pragra.Day12;
 
+import com.pragra.Day11.Student;
+
 public class Constructor {
     public static void main(String[] args) {
         Circle c=new Circle();
         c.CalcCircumference();
+        Student st1= new Student(1,"w",3);
+
     }
 }
